d709a517da57102f3624954093683be05f5d0f34
paul <pjmalamug@hotmail.com>
2017-12-19 15:05:51 +0800

    fix

--- src/main/java/com/cdg/service/AddressRoute.java
+++ src/main/java/com/cdg/service/AddressRoute.java
@@ -1,15 +1,13 @@
 package com.cdg.service;
 
-import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.logging.log4j.core.config.properties.PropertiesConfiguration;
 
 public class AddressRoute extends RouteBuilder{
 
 	@Override
 	public void configure() throws Exception {
 		from("{{start.endpoint}}")
-		.log("test")
+		.log("${body}")
 		.to("{{end.endpoint}}");
 	}
 
